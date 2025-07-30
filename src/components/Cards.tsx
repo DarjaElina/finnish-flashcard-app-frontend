91abbeb72341a9ad55d57519e78c47a0035afcf0
DarjaElina <daria.elina98@gmail.com>
2025-07-30 14:32:37 +0300

    add check for words type and display fallback

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -7,6 +7,7 @@ import { getExternalWords } from "../services/words";
 export default function Cards() {
   const {
     data: words,
+    isSuccess,
     isLoading,
     isError,
   } = useQuery({
@@ -14,15 +15,13 @@ export default function Cards() {
     queryFn: getExternalWords,
   });
 
+  console.log(words);
+
   if (isLoading) {
     return <Moose text="Loading words..." />;
   }
 
-  if (isError) {
-    return <Moose text="Oops! Something went wrong! 😢" />;
-  }
-
-  if (words.length === 0) {
+  if (isError || words.length === 0 || !Array.isArray(words)) {
     return (
       <Moose text="Oops! I tried fetching words from the cloud, but something went wrong. Maybe the internet moose got tangled? 🫎💻" />
     );
@@ -30,10 +29,16 @@ export default function Cards() {
 
   return (
     <div className="cards-container">
-      <Moose text="These are words from the outside world! Tap a card to flip it. Save the ones you like!" />
-      <div className="cards">
-        {words && words.map((w: Word) => <Card key={w.id} word={w} />)}
-      </div>
+      {isSuccess && Array.isArray(words) && (
+        <>
+          <Moose text="These are words from the outside world! Tap a card to flip it. Save the ones you like!" />
+          <div className="cards">
+            {words.map((w: Word) => (
+              <Card key={w.id} word={w} />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
